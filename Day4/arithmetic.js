dc27d8d4bf5bd8d055af8261093d80b9145a7892
KiranRaut2137 <rautkiran700@gmail.com>
2025-04-06 10:14:03 +0530

    today we learn console and opeations in js

---
+++ Day4/arithmetic.js
@@ -0,0 +1,43 @@
+
+// let a = 20
+// let b = 11
+
+// // addition 
+
+// console.log(a+b)
+
+// // subtraction 
+
+// console.log(a-b)
+
+// // multiplication 
+
+// console.log(a * b)
+
+// // division 
+
+// console.log(a/b)
+
+// // modulus 
+
+
+// let c = 5 
+// let d = 2
+
+// console.log(2 % 1)
+
+
+
+let first_number = 5
+
+let sec_number =  6 
+
+console.log(first_number + sec_number);
+
+console.log(first_number - sec_number);
+
+console.log(first_number * sec_number);
+
+console.log(first_number / sec_number);
+
+console.log(first_number % sec_number)
